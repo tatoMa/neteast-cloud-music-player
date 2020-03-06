fd5f832513cd521d74bbd1b1c59b34a481f48e80
tatoMa <Jason.ma1904@gmail.com>
2020-03-06 11:05:35 +1100

    axios credential attempt fix

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,16 +3,16 @@ import axios from 'axios'
 const getPlayLists = axios.get(`${process.env.VUE_APP_baseURL}/top/playlist`, { withCredentials: true })
   .then((response) => response.data.playlists)
 
-const getPlayListById = (id) => axios.get(`${process.env.VUE_APP_baseURL}/playlist/detail?id=${id}`)
+const getPlayListById = (id) => axios.get(`${process.env.VUE_APP_baseURL}/playlist/detail?id=${id}`, { withCredentials: true })
   .then((response) => response.data.playlist)
 
-const getMusicDetailById = (id) => axios.get(`${process.env.VUE_APP_baseURL}/song/detail?ids=${id}`)
+const getMusicDetailById = (id) => axios.get(`${process.env.VUE_APP_baseURL}/song/detail?ids=${id}`, { withCredentials: true })
   .then((response) => response.data.songs[0])
 
-const getMusicUrlById = (id) => axios.get(`${process.env.VUE_APP_baseURL}/song/url?id=${id}`)
+const getMusicUrlById = (id) => axios.get(`${process.env.VUE_APP_baseURL}/song/url?id=${id}`, { withCredentials: true })
   .then((response) => response.data.data[0])
 
-const getSearchResult = (text) => axios.get(`${process.env.VUE_APP_baseURL}/search?keywords=${text}`)
+const getSearchResult = (text) => axios.get(`${process.env.VUE_APP_baseURL}/search?keywords=${text}`, { withCredentials: true })
   .then((response) => response.data.result.songs)
 
 export {
