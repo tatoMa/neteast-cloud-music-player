5538b9b32792f309aff8872a1b61279b4957cd72
tatoMa <Jason.ma1904@gmail.com>
2020-03-10 20:48:49 +1100

    refactory root js

--- src/App.vue
+++ src/App.vue
@@ -1,99 +1,35 @@
 <template>
   <v-app>
-    <!-- <app-drawer :appDrawerProp="appDrawer" @toggleAppDrawer="toggleAppDrawer"/> -->
+
     <!-- app drawer -->
     <v-navigation-drawer
       v-model="appDrawer"
       app
       clipped
     >
-      <v-list dense shaped>
-        <v-list-item-group v-model="item" color="primary">
+      <AppDrawer/>
+    </v-navigation-drawer><!-- app drawer -->
 
-        <!-- user avatar and info -->
-        <v-list-item disabled>
-          <v-list-item-avatar size=72 class="mx-auto">
-            <img src="./assets/logo.png">
-          </v-list-item-avatar>
-        </v-list-item>
-        <v-list-item disabled>
-          <v-list-item-content>
-            <v-list-item-title class="text-center">tato music</v-list-item-title>
-            <v-list-item-subtitle class="text-center">make your life colorful</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-        <v-divider></v-divider>
+    <!-- app navbar -->
+    <app-navbar @toggleAppDrawer="toggleAppDrawer"/><!-- app navbar -->
 
-        <!-- navigation items -->
-        <v-list-item
-          v-for="item in items"
-          :key="item.text"
-          link
-          :to="item.url"
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>
-              {{ item.text }}
-            </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-divider></v-divider>
-
-        <!-- subcriptions -->
-        <!-- <v-subheader class="mt-4 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader>
-        <v-list>
-          <v-list-item
-            v-for="item in items2"
-            :key="item.text"
-            link
-          >
-            <v-list-item-avatar>
-              <img
-                :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`"
-                alt=""
-              >
-            </v-list-item-avatar>
-            <v-list-item-title v-text="item.text" />
-          </v-list-item>
-        </v-list>
-        <v-divider></v-divider> -->
-        <v-spacer></v-spacer>
-        <!-- bottom buttons -->
-        <v-list-item
-          class="mt-4"
-          link
-        >
-          <v-list-item-action>
-            <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
-          </v-list-item-action>
-          <v-list-item-title class="grey--text text--darken-1">Custom Playlist</v-list-item-title>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon color="grey darken-1">mdi-settings</v-icon>
-          </v-list-item-action>
-          <v-list-item-title class="grey--text text--darken-1">Setting</v-list-item-title>
-        </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-    <app-navbar @toggleAppDrawer="toggleAppDrawer"/>
+    <!-- main section -->
     <v-content>
-    <!-- <a @click="appDrawer = !appDrawer"> click </a> -->
       <v-scroll-x-transition mode="out-in">
+          <!-- router -->
           <router-view></router-view>
       </v-scroll-x-transition>
-    </v-content>
+    </v-content><!-- main section -->
+
+    <!-- bottom player -->
     <Player/>
+
   </v-app>
 </template>
 
 <script>
 // import { mapGetters } from 'vuex'
-// import appDrawer from './components/AppDrawer'
+import AppDrawer from './components/AppDrawer'
 import appNavbar from './components/AppNavbar'
 import Player from './components/Player'
 export default {
@@ -101,24 +37,12 @@ export default {
   components: {
     // appDrawer,
     appNavbar,
+    AppDrawer,
     Player
   },
   data () {
     return {
-      appDrawer: false,
-      item: 2,
-      items: [
-        { icon: 'mdi-fire', text: 'Top Music', url: '/' },
-        { icon: 'mdi-format-list-bulleted-square', text: 'Top Playlists', url: 'topplaylists' },
-        { icon: 'mdi-account-multiple', text: 'Top Artists', url: 'topartist' }
-      ],
-      items2: [
-        { picture: 28, text: 'Joseph' },
-        { picture: 38, text: 'Apple' },
-        { picture: 48, text: 'Xbox Ahoy' },
-        { picture: 58, text: 'Nokia' },
-        { picture: 78, text: 'MKBHD' }
-      ]
+      appDrawer: false
     }
   },
   created () {
@@ -127,9 +51,6 @@ export default {
   methods: {
     toggleAppDrawer () {
       this.appDrawer = !this.appDrawer
-    },
-    gotoPage (url) {
-      this.$router.push(`/${url}`)
     }
   }
 }
@@ -137,8 +58,5 @@ export default {
 <style>
 .cursor-pointer{
   cursor: pointer;
-}
-.v-messages{
-  min-height: 0px !important
 }
 </style>
---
+++ src/components/AppDrawer.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-list dense shaped>
+    <v-list-item-group v-model="item" color="primary">
+
+    <!-- user avatar and info -->
+    <v-list-item disabled>
+      <v-list-item-avatar size=72 class="mx-auto">
+        <img src="../assets/logo.png">
+      </v-list-item-avatar>
+    </v-list-item>
+    <v-list-item disabled>
+      <v-list-item-content>
+        <v-list-item-title class="text-center">tato music</v-list-item-title>
+        <v-list-item-subtitle class="text-center">make your life colorful</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+
+    <!-- navigation items -->
+    <v-list-item
+      v-for="item in items"
+      :key="item.text"
+      link
+      :to="item.url"
+    >
+      <v-list-item-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title>
+          {{ item.text }}
+        </v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+    <v-divider></v-divider>
+    <v-spacer></v-spacer>
+    <!-- bottom buttons -->
+    <v-list-item
+      class="mt-4"
+      link
+    >
+      <v-list-item-action>
+        <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
+      </v-list-item-action>
+      <v-list-item-title class="grey--text text--darken-1">Custom Playlist</v-list-item-title>
+    </v-list-item>
+    <v-list-item link>
+      <v-list-item-action>
+        <v-icon color="grey darken-1">mdi-settings</v-icon>
+      </v-list-item-action>
+      <v-list-item-title class="grey--text text--darken-1">Setting</v-list-item-title>
+    </v-list-item>
+    </v-list-item-group>
+  </v-list>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      item: 2,
+      items: [
+        { icon: 'mdi-fire', text: 'Top Music', url: '/' },
+        { icon: 'mdi-format-list-bulleted-square', text: 'Top Playlists', url: 'topplaylists' }
+        // { icon: 'mdi-account-multiple', text: 'Top Artists', url: 'topartist' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
