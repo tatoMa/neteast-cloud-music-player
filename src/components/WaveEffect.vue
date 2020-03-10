2c7f4ddd4c55ecf38812965c4418a43b0c509689
tatoMa <Jason.ma1904@gmail.com>
2020-03-10 20:59:17 +1100

    wave effect update

--- src/components/WaveEffect.vue
+++ src/components/WaveEffect.vue
@@ -77,24 +77,24 @@ export default {
     }
 }
 
-.bar:nth-child(1)  { left: 5%; animation-duration: 474ms; }
-.bar:nth-child(2)  { left: 10%; animation-duration: 433ms; }
-.bar:nth-child(3)  { left: 15%; animation-duration: 407ms; }
-.bar:nth-child(4)  { left: 20%; animation-duration: 458ms; }
-.bar:nth-child(5)  { left: 25%; animation-duration: 400ms; }
-.bar:nth-child(6)  { left: 30%; animation-duration: 427ms; }
-.bar:nth-child(7)  { left: 35%; animation-duration: 441ms; }
-.bar:nth-child(8)  { left: 40%; animation-duration: 419ms; }
-.bar:nth-child(9)  { left: 45%; animation-duration: 487ms; }
-.bar:nth-child(10) { left: 50%; animation-duration: 442ms; }
-.bar:nth-child(11)  { left: 55%; animation-duration: 474ms; }
-.bar:nth-child(12)  { left: 60%; animation-duration: 433ms; }
-.bar:nth-child(13)  { left: 65%; animation-duration: 407ms; }
-.bar:nth-child(14)  { left: 70%; animation-duration: 458ms; }
-.bar:nth-child(15)  { left: 75%; animation-duration: 400ms; }
-.bar:nth-child(16)  { left: 80%; animation-duration: 427ms; }
-.bar:nth-child(17)  { left: 85%; animation-duration: 441ms; }
-.bar:nth-child(18)  { left: 90%; animation-duration: 419ms; }
-.bar:nth-child(19)  { left: 95%; animation-duration: 487ms; }
-.bar:nth-child(20) { left: 100%; animation-duration: 442ms; }
+.bar:nth-child(1)  { left: 5%; animation-duration: 374ms; }
+.bar:nth-child(2)  { left: 10%; animation-duration: 333ms; }
+.bar:nth-child(3)  { left: 15%; animation-duration: 307ms; }
+.bar:nth-child(4)  { left: 20%; animation-duration: 358ms; }
+.bar:nth-child(5)  { left: 25%; animation-duration: 300ms; }
+.bar:nth-child(6)  { left: 30%; animation-duration: 327ms; }
+.bar:nth-child(7)  { left: 35%; animation-duration: 341ms; }
+.bar:nth-child(8)  { left: 40%; animation-duration: 319ms; }
+.bar:nth-child(9)  { left: 45%; animation-duration: 387ms; }
+.bar:nth-child(10) { left: 50%; animation-duration: 342ms; }
+.bar:nth-child(11)  { left: 55%; animation-duration: 374ms; }
+.bar:nth-child(12)  { left: 60%; animation-duration: 333ms; }
+.bar:nth-child(13)  { left: 65%; animation-duration: 307ms; }
+.bar:nth-child(14)  { left: 70%; animation-duration: 358ms; }
+.bar:nth-child(15)  { left: 75%; animation-duration: 300ms; }
+.bar:nth-child(16)  { left: 80%; animation-duration: 327ms; }
+.bar:nth-child(17)  { left: 85%; animation-duration: 341ms; }
+.bar:nth-child(18)  { left: 90%; animation-duration: 319ms; }
+.bar:nth-child(19)  { left: 95%; animation-duration: 387ms; }
+.bar:nth-child(20) { left: 100%; animation-duration: 342ms; }
 </style>
