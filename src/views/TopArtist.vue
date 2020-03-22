0eb12bb4e1a99f8e59850eb66bdea8e8816764cd
tatoMa <Jason.ma1904@gmail.com>
2020-03-22 22:09:19 +1100

    artist update

--- src/views/TopArtist.vue
+++ src/views/TopArtist.vue
@@ -1,9 +1,19 @@
 <template>
     <div>
-      <div v-for="artist in getTopArtist" :key="artist.id">
-        {{artist.name}}
-        <v-img :src="artist.picUrl + '?param=400y400'"></v-img>
-      </div>
+      <v-card
+        v-for="artist in getTopArtist" :key="artist.id"
+        class="mx-auto my-3"
+        max-width="400"
+      >
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          :src="artist.picUrl + '?param=400y400'"
+          :alt="artist.name"
+        >
+          <v-card-title>{{artist.name}}</v-card-title>
+        </v-img>
+      </v-card>
     </div>
 </template>
 
