76fb05aec497a20f6f1937a0a08c1516991fbea6
tatoMa <Jason.ma1904@gmail.com>
2020-04-17 20:10:27 +1000

    add capacitor

--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 
+// import { Plugins } from 'capacitor'
+// const { SplashScreen } = Plugins
+
 Vue.config.productionTip = false
 
 new Vue({
@@ -13,4 +16,7 @@ new Vue({
   store,
   vuetify,
   render: h => h(App)
+  // mounted () {
+  //   SplashScreen.hide()
+  // }
 }).$mount('#app')
